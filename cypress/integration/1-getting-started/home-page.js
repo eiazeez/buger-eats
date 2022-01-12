b1a4d122abb3a48641c2a8375b7e114912c5abd4
byazeez <isaac.douglas08@gmail.com>
2022-01-12 11:43:23 -0300

    Ajustando a suite home para os padrões CYPRESS.JSON

--- cypress/integration/1-getting-started/home-page.js
+++ cypress/integration/1-getting-started/home-page.js
@@ -3,9 +3,7 @@
   describe('home page', ()=>{
 
     it('app deve estar funcionando', ()=>{
-
-        cy.viewport(1366, 768)
-        cy.visit('https://buger-eats.vercel.app/')
+        cy.visit('/')
         cy.get('#page-home main h1')
 
     })
