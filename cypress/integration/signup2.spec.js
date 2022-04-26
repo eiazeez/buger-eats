df8865c98672e64396fba8d99673d371e6adf1b7
Isaac Douglas Aragão <isaac.aragao@codeby.com.br>
2022-04-26 13:28:43 -0300

    Integration Update

--- cypress/integration/signup2.spec.js
+++ cypress/integration/signup2.spec.js
@@ -46,13 +46,13 @@ describe('Signup', () => {
 
         const messages = [
 
-            { field: 'name', output: 'É necessário informar o nome' },
-            { field: 'cpf', output: 'É necessário informar o CPF' },
-            { field: 'email', output: 'É necessário informar o email' },
-            { field: 'postalcode', output: 'É necessário informar o CEP' },
-            { field: 'number', output: 'É necessário informar o número do endereço' },
+            { field: 'name',            output: 'É necessário informar o nome' },
+            { field: 'cpf',             output: 'É necessário informar o CPF' },
+            { field: 'email',           output: 'É necessário informar o email' },
+            { field: 'postalcode',      output: 'É necessário informar o CEP' },
+            { field: 'number',          output: 'É necessário informar o número do endereço' },
             { field: 'delivery-method', output: 'Selecione o método de entrega' },
-            { field: 'cnh', output: 'Adicione uma foto da sua CNH' }
+            { field: 'cnh',             output: 'Adicione uma foto da sua CNH' }
 
         ]
 
